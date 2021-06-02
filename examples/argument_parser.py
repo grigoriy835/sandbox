0db7766ee0d282b8cfd72c597cb46deab8da6f61
Dmitrii Grigorev <dmitrii_grigorev1@epam.com>
2021-06-03 02:12:50 +0300

    update argparse example

--- examples/argument_parser.py
+++ examples/argument_parser.py
@@ -1,13 +1,13 @@
 import argparse
 
 
-parser = argparse.ArgumentParser('apply or revert migration')
-parser.add_argument('name', help='shit')
+parser = argparse.ArgumentParser('example for argparse')
+parser.add_argument('name', help='shit', default='default name')
 parser.add_argument('-down', '-d', action='store_const', const='down', default='up',
                     help='if this setted revert migrations')
 parser.add_argument('-count', type=int, default=0)
-parser.add_argument('-auto-confirm', '-c', action='store_true', default=False)
+parser.add_argument('-auto-confirm', '-c', action='store_true', dest='dest', default=False)
 
 if __name__ == '__main__':
     arguments = parser.parse_args()
-    print((arguments.name, arguments.down, arguments.count, arguments.auto_confirm))
+    print(('name ', arguments.name, ', flag 1 ', arguments.down, ', flag 2 ', arguments.count, ', auto confitm ', arguments.dest))
