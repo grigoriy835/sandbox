db7cb02664ffd343166f9e78d837886c0c528d58
grisha <needmoreicecream@gmail.com>
2021-07-17 16:21:33 +0300

    cleanup and normalize repo

--- latentmeter/server.py
+++
@@ -1,9 +0,0 @@
-from aiohttp import web
-
-app = web.Application()
-app['instance_prefix'] = ''
-
-
-def run_server(host, port):
-    app['instance_prefix'] = port
-    web.run_app(app, host=host, port=port)
